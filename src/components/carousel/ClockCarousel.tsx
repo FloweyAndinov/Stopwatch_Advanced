d1c386c9a879d8bf1c2d04ba4d8bbbf40e5f2476
Tsvetan Andinov <flowey.andinov@gmail.com>
2024-12-29 02:33:25 +0200

    Feature : infinite clock generation
    onclickcapture to save the day :3

--- src/components/carousel/ClockCarousel.tsx
+++ src/components/carousel/ClockCarousel.tsx
@@ -1,26 +1,56 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '../ui/carousel'
 import Clock from '../clockComponents/ClockParent'
 import { HelpCircle } from 'lucide-react'
 import { Input } from '../ui/input'
 import ClockParent from '../clockComponents/ClockParent'
+import useEmblaCarousel from 'embla-carousel-react'
 
 function ClockCarousel() {
+
+  const [leng, setLeng] = React.useState(2)
+  const [currSlide, setCurrSlide] = React.useState(0)
+  const [emblaRef, emblaApi] = useEmblaCarousel()
+  function slideGeneration(action: string) {
+   switch (action) {
+     case 'next':
+      setCurrSlide(prev => prev + 1)
+      if (currSlide === leng - 2) {
+        setLeng(prev => prev + 1)
+      }
+      break;
+     case 'prev':
+      setCurrSlide(prev => prev -1)
+      break;
+   }
+  }
+
+  useEffect(() => { 
+       if (emblaApi) 
+        emblaApi.on('slidesInView', () => {
+        //   console.log(emblaApi.selectedScrollSnap())
+        //   expandCarousel(emblaApi.selectedScrollSnap())
+        console.log(emblaApi.slideNodes())
+        })
+        },
+        
+         // eslint-disable-next-line react-hooks/exhaustive-deps
+         [emblaApi])
   return (
     <div>
-        <Carousel className="max-w-3xl">
+        <Carousel className="max-w-3xl" opts={{ dragFree: false, }} ref={emblaRef}>
       <CarouselContent>
-        {Array.from({ length: 2 }).map((_, index) => (
-          <CarouselItem key={index}>
+        {Array.from({ length: leng }).map((_, index) => (
+          <CarouselItem key={index} >
             <div style={{width:'100%', height:'100%', display:'flex', flexFlow:'column', justifyContent:'center', alignItems:'center'}}>
               <ClockParent/>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious  style={{width:'3rem', height:'3rem' }}/>
-      <CarouselNext style={{width:'3rem', height:'3rem' }}/>
+      <CarouselPrevious  style={{width:'3rem', height:'3rem' }} onClickCapture={() => slideGeneration('prev')}/>
+      <CarouselNext style={{width:'3rem', height:'3rem' }} onClickCapture={() => slideGeneration('next')}/>
     </Carousel>
     </div>
   )
